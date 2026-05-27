74c98ab5d0b73cf40d773496262823bfad5c4b51
gfalvo1968-create <gfalvo1968@icloud.com>
2026-05-27 15:07:10 -0400

    Update hall.js

--- hall.js
+++ hall.js
@@ -20,8 +20,8 @@ function createHallSignal(){
     label.textContent =
         hallSignals[Math.floor(Math.random() * hallSignals.length)];
 
-    const x = Math.random() * 50 + 25;
-    const y = Math.random() * 45 + 38;
+    const x = Math.random() * 36 + 32;
+    const y = Math.random() * 32 + 42;
 
     blip.style.left = x + "%";
     blip.style.top = y + "%";
